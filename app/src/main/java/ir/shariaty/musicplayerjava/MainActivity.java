7f9a53f51491d6da32cd5368288d4914be74256b
Zeinab Ahmadi <z.ahmadi9883@gmail.com>
2025-05-12 10:42:08 +0330

    second commit

--- app/src/main/java/ir/shariaty/musicplayerjava/MainActivity.java
+++ app/src/main/java/ir/shariaty/musicplayerjava/MainActivity.java
@@ -17,7 +17,7 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView play,prev,next;
+    ImageView play,prev,next,ImageView;
     TextView songTitle;
     SeekBar mSeekBarTime, mSeekBarVol;
     static MediaPlayer mMediaPlayer;
@@ -38,8 +38,8 @@ public class MainActivity extends AppCompatActivity {
 
         ArrayList<Integer> songs = new ArrayList<>();
 
-        songs.add(0 , R.raw.Man);
-        songs.add(1 , R.raw.B);
+        songs.add(0 , R.raw.ahmadi);
+        songs.add(1 , R.raw.zeinab);
 
         // initialize MediaPlayer
         mMediaPlayer = MediaPlayer.create(getApplicationContext(),songs.get(currentIndex));
@@ -50,13 +50,67 @@ public class MainActivity extends AppCompatActivity {
                 if (mMediaPlayer != null && mMediaPlayer.isPlaying()){
                     mMediaPlayer.pause();
                     play.setImageResource(R.drawable.ic_play_arrow_24);
+                } else {
+                    mMediaPlayer.start();
+                    play.setImageResource(R.drawable.ic_pause_24);
+                }
+                if (currentIndex < songs.size() - 1){
+                    currentIndex++;
+                } else{
+                    currentIndex=0;
+                }
+                if (mMediaPlayer.isPlaying()){
+                    mMediaPlayer.stop();
+                }
+                mMediaPlayer = mMediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+                mMediaPlayer.start();
+                SongsName();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mMediaPlayer!=null){
+                    play.setImageResource(R.drawable.ic_pause_24);
                 }
             }
         });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mMediaPlayer != null) {
+                    play.setImageResource(R.drawable.ic_pause_24);
+                }
+                if (currentIndex > 0) {
+                    currentIndex--;
+                } else {
+                    currentIndex = songs.size() - 1;
+                }
+                if (mMediaPlayer.isPlaying()) {
+                    mMediaPlayer.stop();
+                }
+                mMediaPlayer = mMediaPlayer.create(getApplicationContext(), songs.get(currentIndex));
+                mMediaPlayer.start();
+                SongsName();
+            }
+        });
+
+                ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void SongsName() {
+        if (currentIndex==0){
+            songTitle.setText("zeinab-nemidunam");
+            ImageView.setImageResource(R.drawable.zeinab);
+        } else if (currentIndex==1) {
+            songTitle.setText("man-yadam nist");
+            ImageView.setImageResource(R.drawable.ahmadi);
+        }
     }
 }
